316f85a8dccf169787f21c4aca31fa416f7c4cb2
sjolundmartin <martinsjolund@hotmail.com>
2024-08-19 10:17:08 +0200

    authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 const INVOICES_URL = "/dashboard/invoices";
 
@@ -115,5 +117,24 @@ export async function deleteInvoice(id: string) {
     return { message: "Deleted invoice" };
   } catch (error) {
     return { message: "Database error: Failed to delete invoice" };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
   }
 }
